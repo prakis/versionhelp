a250c8440693998afab1caddd3e1b6e005851091
Krishna <pra_kis@hotmail.com>
2019-12-25 20:34:31 +0530

    First working version with setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+setup(
+  name = 'versionhelp',
+  packages = ['versionhelp'], # this must be the same as the name above
+
+  version = '0.3',
+  description = 'A Simple Version Command', 
+  author = 'Kishore',
+  author_email = 'prakis@gmail.com',
+
+  license='MIT',
+  url = 'https://github.com/prakis/versionhelp', # use the URL to the github repo
+  download_url = 'https://prakis.github.io/versionhelp/archive/versionhelp-0.3.tar.gz',
+  keywords = ['version', 'versionhelp'], # arbitrary keywords
+  classifiers = [],
+)
